567e161381b1bcb24c19975073cb692860f7d894
BrainSop <brainsop@gmail.com>
2019-07-21 15:41:26 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,9 +8,7 @@ git --version
 cp /data/options.json ring-alarm-mqtt/config.json
 
 cd ring-alarm-mqtt
-echo antes de ls
 npm install
 npm audit fix
 chmod a+x ring-alarm-mqtt.js
-echo Antes de Debug!
 DEBUG=* /ring-alarm-mqtt/ring-alarm-mqtt.js
