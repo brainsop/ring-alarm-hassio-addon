ae0036a09f5c5709dc3e422fe3cd7bf3be58aeef
BrainSop <brainsop@gmail.com>
2019-07-21 15:33:35 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,19 +3,14 @@ echo Node Version
 node -v
 echo NPM Version
 npm -v
-echo antes de git--version
 git --version
 
-echo antes de copiar
 cp /data/options.json ring-alarm-mqtt/config.json
-cat ring-alarm-mqtt/config.json
-echo arriba deberia aparecer la verificacion de file
 
 cd ring-alarm-mqtt
 echo antes de ls
-ls -l
-echo se ejecuto ls?
 npm install
 npm audit fix
 chmod a+x ring-alarm-mqtt.js
+echo Antes de Debug!
 DEBUG=* /ring-alarm-mqtt/ring-alarm-mqtt.js
